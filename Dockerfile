b19ff7370af5eaae15011e8db31e97da45b00e5c
Tony Alexander Hild <tony_hild@yahoo.com>
2017-08-14 11:32:50 -0300

    Cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,13 @@
 # VERSION 0.1
 
-FROM ubuntu:xenial
+FROM decomp/boca-base
 
-RUN apt-get -y update
-RUN apt-get -y install tzdata locales software-properties-common python-software-properties
-RUN apt-get -y upgrade
-RUN echo "America/Sao_Paulo" > /etc/timezone; dpkg-reconfigure -f noninteractive tzdata
-RUN locale-gen en_US en_US.UTF-8 pt_BR.UTF-8
-RUN locale -a
-ENV LANGUAGE=pt_BR.UTF-8
-ENV LANG=pt_BR.UTF-8
-ENV LC_ALL=pt_BR.UTF-8
-RUN locale-gen pt_BR.UTF-8
-RUN DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+RUN apt-get -y install quota accountsservice --no-install-recommends --allow-unauthenticated
+RUN apt-get -y clean
 
-RUN add-apt-repository ppa:brunoribas/ppa-maratona
-RUN apt-get -y update
-RUN apt-get -y install maratona-boca
-
-COPY init.sh /init.sh
+COPY startup.sh /startup.sh
 
 WORKDIR /var/www/boca
 
 # Execute the containers startup script which will start many processes/services
-CMD ["/bin/bash", "/init.sh"]
-
-
-
+CMD ["/bin/bash", "/startup.sh"]
---
+++ startup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if ! grep -q bdserver=$DBHOST /etc/boca.conf; then
+
+    touch ~/.pgpass
+    echo $DBHOST:5432:*:$DBUSER:$DBPASS > ~/.pgpass
+    chmod 600 ~/.pgpass
+
+    until [[ $( psql -h "$DBHOST" -U "$DBUSER" -tAc "SELECT 1 FROM pg_database WHERE datname='$DBNAME'" ) == "1" ]]; do
+        >&2 echo "Database not created - sleeping"
+        sleep 5
+    done
+
+    echo 'bdcreated=y' >> /etc/boca.conf
+
+    . /etc/boca.conf
+
+    echo "bdserver=$DBHOST" >> /etc/boca.conf
+
+    privatedir=$bocadir/src/private
+
+    PASSK=`makepasswd --chars 20`
+    awk -v dbhost="$DBHOST" -v pass="$DBPASS" -v passk="$PASSK" '{ if(index($0,"[\"dbpass\"]")>0) \
+    print "$conf[\"dbpass\"]=\"" pass "\";"; \
+    else if(index($0,"[\"dbhost\"]")>0) print "$conf[\"dbhost\"]=\"" dbhost "\";"; \
+    else if(index($0,"[\"dbsuperpass\"]")>0) print "$conf[\"dbsuperpass\"]=\"" pass "\";"; \
+    else if(index($0,"[\"key\"]")>0) print "$conf[\"key\"]=\"" passk "\";"; else print $0; }' \
+    < $privatedir/conf.php > $privatedir/conf.php1
+    mv -f $privatedir/conf.php1 $privatedir/conf.php
+
+
+    boca-createjail
+fi
+
+tail -f /dev/null
